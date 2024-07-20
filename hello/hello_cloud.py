9cba2d2832d59a337d9918c2ff112b663f55669f
Knirag <k.niragire@alustudent.com>
2024-07-20 01:24:03 +0000

    Add shebang to hello_cloud file

--- hello/hello_cloud.py
+++ hello/hello_cloud.py
@@ -2,9 +2,18 @@
 
 import http.server
 import socketserver
+import sys
 
+# Default port
 PORT = 8000
 
+# Check if a port number is provided as an argument
+if len(sys.argv) > 1:
+    try:
+        PORT = int(sys.argv[1])
+    except ValueError:
+        print("Invalid port number. Using default port 8000.")
+        
 class HelloHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -15,7 +24,7 @@ class HelloHandler(http.server.SimpleHTTPRequestHandler):
 # Create an object of the above class
 handler_object = HelloHandler
 
-# Create a TCP socket to listen on port 8000
+# Create a TCP socket to listen on the specified port
 with socketserver.TCPServer(("", PORT), handler_object) as httpd:
     print(f"Serving HTTP on port {PORT}...")
     httpd.serve_forever()
